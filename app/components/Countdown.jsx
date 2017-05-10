7d0710ac7845d0b14aba1f1af6640f46ac85090b
Shane Goodson <shane@webzai.com>
2017-05-10 08:07:59 +0300

    Set up countdown input form

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import Clock from 'Clock';
+import CountdownForm from 'CountdownForm';
 
 export default class WeatherForm extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+    }
+
+    handleSetCountdown = (seconds) => {
+        this.setState({
+            count: seconds
+        });
+    }
+
     render(){
+        const {count} = this.state;
         return (
             <div>
-                <Clock totalSeconds={129}/>
+                <Clock totalSeconds={count}/>
+                <CountdownForm onSetCountDown={this.handleSetCountdown}/>
             </div>
         )
     }
